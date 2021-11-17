6e195edcd8ffd2a098953d162e67b5ce0e6313a9
César Hunault <cesarhunault@gmail.com>
2021-11-17 20:25:34 +0100

    premier jet

--- identification.php
+++ identification.php
@@ -4,93 +4,62 @@
 <head>
     <title>Projet LGS: identification</title>
     <meta charset="utf-8">
-    <link   href="css/bootstrap.min.css" rel="stylesheet">
-    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="identification.css"/>
     <script language="javascript">
         function foncton() {
-            if (document.getElementById('img').className=="icon-eye-open"){
+            if (document.getElementById('img').className=="fas fa-eye"){
                 document.getElementById('mdp').type="text";
-                document.getElementById('img').className="icon-eye-close";
+                document.getElementById('img').className="fas fa-eye-slash";
             }else{
                 document.getElementById('mdp').type="password";
-                document.getElementById('img').className="icon-eye-open";
+                document.getElementById('img').className="fas fa-eye";
             }
         }
     </script>
+    <script src="https://kit.fontawesome.com/2e82b0a3e4.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <div class="container">
-    		<div class="row">
-                <table border=0px>
-                    <tr>
-                        <td>
-                            <img
-                            src="img/LogoDB.png"
-                            alt="Logo du projet"
-                            width=200px>
-                        </td>
-                        <td>
-    	           		    <h1>Page d'identification</h1>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                        </td>
-                        <td>
-                            <form method="post">
-                                <table border=1px cellpadding=5px rules=none>
-                                    <tr>
-                                        <td>
-                                            <p>  Identifiant  
-                                            </p>
-                                        </td>
-                                        <td>
-                                            <input type="text" name="id" />
-                                        </td>
-                                        <td>
-                                        </td>
-                                    </tr>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <p>  Mot de passe  
-                                            </p>
-                                        </td>
-                                        <td>
-                                            <input type="password" placeholder="Mot de passe" id="mdp" name="mdp2" />
-                                        </td>
-                                        <td>
-                                            <p><i class="icon-eye-open" id="img" onclick="foncton();" onmouseover="document.getElementById('img').innerHTML = 'Révéler le mot de passe'" onmouseout="document.getElementById('img').innerHTML = ''"></i></p>
-
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                        </td>
-                                        <td>
-                                            <center><input type="submit" value="Se connecter" /></center>                                         
-                                        </td>
-                                    </tr>
-                                </table>
-
-                                <?php
-                                    include_once("myparam.inc.php");
-                                    if (!empty($_POST)){
-                                        if ($_POST["id"]==MYUSER && $_POST["mdp2"]==MYPASS){
-                                            header('Location: index2.php');
-                                            exit();
-                                        }
-                                        else{
-                                            echo "<script>alert(\"L'identifiant et/ou le mot de passe est/sont erroné(s)\")</script>";
-                                        }
-                                    }
-                                ?>
-                            </form>
-                        </td>
-                    </tr>
-		        </table>
+    <div class="fullscreen-container parent">
+        <video autoplay loop muted>
+            <source src="test-perso/pexels-tima-miroshnichenko-6722193.mp4" type="video/mp4"/>
+        </video>
+        <div class="container child">
+            <div class="img-presentation">
+                <img src="img/LogoDB.png" alt="Logo du projet">
             </div>
-    </div> 
-  </body>
+            <div class="formulaire">                     
+                <form method="post">
+                    <div class="input-group">
+                        <label>Identifiant</label>
+                        <input type="text" name="id" required>
+                    </div>
+                   <div class="input-group">
+                       <div class="alignement">
+                            <label>Mot de passe</label>
+                            <div class="font-icon">
+                                <i class="fas fa-eye" id="img" onclick="foncton();" onmouseover="document.getElementById('img').innerHTML = 'Révéler le mot de passe'" onmouseout="document.getElementById('img').innerHTML = ''"></i>
+                            </div>  
+                        </div>
+                        <input type="password" id="mdp" name="mdp2" required>
+                    </div>
+                    
+                    <button type="submit" class="login-button">Se connecter</button>                                        
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+<?php
+    include_once("myparam.inc.php");
+    if (!empty($_POST)){
+        if ($_POST["id"]==MYUSER && $_POST["mdp2"]==MYPASS){
+            header('Location: index2.php');
+            exit();
+        }
+        else{
+            echo "<script>alert(\"L'identifiant et/ou le mot de passe est/sont erroné(s)\")</script>";
+        }
+    }
+?>
 </html>
